4755c66b400ebbd5e63258d8445831c178c51829
vermin <vermin@techie.com>
2019-10-12 05:20:41 +0200

    done done and done 🎈

--- utils/doxygen-publish.sh
+++ utils/doxygen-publish.sh
@@ -19,4 +19,4 @@ if [[ ! -d "$wwwdir/doc" ]] ; then
 fi
 
 echo "Generating:"
-doxygen Doxyfile && echo "Backup previous version:" && rm -rf ~/wazn-www-previous && mv "$wwwdir/doc" ~/wazn-www-previous && cp -ar doc/ "$wwwdir/" && echo "Done, builded and copied to public - the doxygen docs" && echo "size:" && du -Dsh "$wwwdir/" && echo "files:" && find "$wwwdir/" | wc -l
+doxygen Doxyfile && echo "Backup previous version:" && rm -rf ~/wazn-www-previous && mv "$wwwdir/doc" ~/wazn-www-previous && cp -ar doc/ "$wwwdir/" && echo "Done, built and copied to public - the doxygen docs" && echo "size:" && du -Dsh "$wwwdir/" && echo "files:" && find "$wwwdir/" | wc -l
