98117e8c1fdaf1e60b3156af848c748491dea3f7
vermin <vermin@techie.com>
2019-10-17 08:28:13 +0200

    test!

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -85,9 +85,9 @@
 #define DIFFICULTY_CUT                                  60  // timestamps to cut after sorting
 #define DIFFICULTY_BLOCKS_COUNT                         (DIFFICULTY_WINDOW + DIFFICULTY_LAG)
 
-#define DIFFICULTY_WINDOW_V2				120
-#define DIFFICULTY_CUT_V2				6
-#define DIFFICULTY_BLOCKS_COUNT_V2			(DIFFICULTY_WINDOW_V2 + 1)
+#define DIFFICULTY_WINDOW_V2			                     	120
+#define DIFFICULTY_CUT_V2		                         		6
+#define DIFFICULTY_BLOCKS_COUNT_V2		                	(DIFFICULTY_WINDOW_V2 + 1)
 
 #define CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V1   DIFFICULTY_TARGET_V1 * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS
 #define CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V2   DIFFICULTY_TARGET_V2 * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS
@@ -101,8 +101,8 @@
 #define BLOCKS_SYNCHRONIZING_DEFAULT_COUNT_PRE_V4       200    //by default, blocks count in blocks downloading
 #define BLOCKS_SYNCHRONIZING_DEFAULT_COUNT              200     //by default, blocks count in blocks downloading
 
-#define CRYPTONOTE_MEMPOOL_TX_LIVETIME                    (86400*3) //seconds, three days
-#define CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME     604800 //seconds, one week
+#define CRYPTONOTE_MEMPOOL_TX_LIVETIME                  (86400*3) //seconds, three days
+#define CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME   604800 //seconds, one week
 
 #define COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT           1000
 
@@ -170,9 +170,9 @@ namespace config
   uint16_t const RPC_DEFAULT_PORT = 11787;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 11788;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+      0x12 ,0x88, 0xF1, 0x33 , 0x61, 0x04 , 0xbb, 0x44, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xff, 0x11
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d9";
+  std::string const GENESIS_TX = "";
   uint32_t const GENESIS_NONCE = 10000;
 
   namespace testnet
@@ -184,9 +184,9 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 22787;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 22788;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
+        0x12 ,0x88, 0xF1, 0x33 , 0x61, 0x04 , 0xbb, 0x44, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xff, 0x12
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d9";
+    std::string const GENESIS_TX = "";
     uint32_t const GENESIS_NONCE = 10001;
   }
 
@@ -199,9 +199,9 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 33787;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 33788;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x12
+        0x12 ,0x88, 0xF1, 0x33 , 0x61, 0x04 , 0xbb, 0x44, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xff, 0x13
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff0302df5d56da0c7d643ddd1ce61901c7bdc5fb1738bfe39fbe69c28a3a7032729c0f2101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
+    std::string const GENESIS_TX = "";
     uint32_t const GENESIS_NONCE = 10002;
   }
 }
