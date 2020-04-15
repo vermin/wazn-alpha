f73cd07df115cd3f567d709186387c0db619664c
vermin <vermin@techie.com>
2020-04-15 23:16:16 +0200

    added buffer

--- contrib/epee/src/hex.cpp
+++ contrib/epee/src/hex.cpp
@@ -86,7 +86,7 @@ namespace epee
     return write_hex(out, src);
   }
 
-  std::vector<uint8_t> from_hex::vector(boost::string_ref src)
+  std::vector<uint8_t> from_hex::vector(const boost::string_ref src)
   {
     // should we include a specific character
     auto include = [](char input) {
@@ -106,7 +106,7 @@ namespace epee
     result.reserve(count / 2);
 
     // the data to work with (std::string is always null-terminated)
-    auto data = src.data();
+    auto data = src.begin();
 
     // convert a single hex character to an unsigned integer
     auto char_to_int = [](const char *input) {
@@ -132,9 +132,9 @@ namespace epee
     };
 
     // keep going until we reach the end
-    while (data[0] != '\0') {
+    while (data != src.end()) {
       // skip unwanted characters
-      if (!include(data[0])) {
+      if (!include(*data)) {
         ++data;
         continue;
       }
