bb58ba36f30de118d76e8eeaf6ca282c17740422
vermin <vermin@techie.com>
2020-04-16 08:17:07 +0200

    blobdata_ref

--- src/cryptonote_basic/blobdatatype.h
+++ src/cryptonote_basic/blobdatatype.h
@@ -1,5 +1,4 @@
-// Copyright (c) 2019 WAZN Project
-// Copyright (c) 2018 uPlexa Team
+// Copyright (c) 2019-2020 WAZN Project
 // Copyright (c) 2014-2018 The Monero Project
 //
 // All rights reserved.
@@ -32,7 +31,11 @@
 
 #pragma once
 
+#include <string>
+#include "span.h"
+
 namespace cryptonote
 {
   typedef std::string blobdata;
+  typedef epee::span<const char> blobdata_ref;
 }
